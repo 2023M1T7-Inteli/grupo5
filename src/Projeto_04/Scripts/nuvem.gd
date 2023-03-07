530a7b0b0595cb1c4ea29cac8ca23620158823c6
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-03-07 13:49:19 -0300

    up

--- src/Projeto_04/Scripts/nuvem.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite
-	
-#Aqui é o movimento da nuvem. Quando chegar em certa posição ela irá retornar.
-func _process(delta):
-	position.x -= 0.5
-	if position.x <= 565:
-		position.x = 3308
-
