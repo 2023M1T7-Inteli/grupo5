685aad51843054a59a90ab04cc78e7347ca2e247
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-03-09 22:02:10 -0300

    0.5.1

---
+++ src/Projeto_05.1/Scripts/itemepersonagem.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+#Aqui estão os cógios da interação. Onde o item coletável irá transportar o jogador para outra tela (portal).
+
+#Função que corresponde ao contato entre os corpos (personagem e item).
+func _on_item1_body_entered(body):
+	#Se haver contato entre o item e objetos do grupo "Player", que nesse caso é o jogador, haverá uma interação. Nesse caso, a troca de mapa.
+	if body.is_in_group("Player"):
+#		body.item = true
+#		queue_free()
+		get_tree().change_scene("res://Cenas/desafio1.tscn")
+
