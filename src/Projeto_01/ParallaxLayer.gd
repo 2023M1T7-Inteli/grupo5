05ae286765b9e2e95e3e9ea376e932e8f06b5b1c
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-03-09 22:05:24 -0300

    0

--- src/Projeto_01/ParallaxLayer.gd
+++
@@ -1,8 +0,0 @@
-extends ParallaxLayer
-
-export var cloud_speed := -100
-
-func _process(delta):
-	motion_offset.x += cloud_speed*delta
-	if position.x == 1000:
-		position.x = 3370
--- src/Projeto_01/nuvem.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite
-	
-func _process(delta):
-	position.x -= 0.5
-	if position.x <= 565:
-		position.x = 3308
