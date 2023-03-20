f28815c2b6267bed0833270a04b2cb6596e3275e
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-03-20 14:37:29 -0300

    0.3

---
+++ src/Projeto_03/Scripts/paulo.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+var toqueDireita = false
+var toqueBaixo = false
+var toqueCima = false
+var toqueEsquerda = false
+var movimento2 = Vector2.ZERO
+var saida: bool = false
+	
+func _physics_process(delta):
+	if toqueBaixo == false and toqueCima == false and toqueEsquerda == false and toqueDireita == false:
+		movimento2.x = 0
+		movimento2.y = 0
+	
+	if toqueDireita:
+		movimento2.x = 500
+		toqueDireita = false
+		
+	if toqueBaixo:
+		movimento2.y = 500
+		toqueBaixo = false
+		
+	if toqueCima:
+		movimento2.y = -500
+		toqueCima = false
+		
+	if toqueEsquerda:
+		movimento2.x = -500
+		toqueEsquerda = false
+		
+	move_and_collide(movimento2)
+	
+	
+		
+# Adicione esse código ao script do objeto que deseja controlar
+# Variável que armazena a velocidade do objeto
+
+func _on_baixo_pressed():
+	toqueBaixo = true
+	
+func _on_direita_pressed():
+	toqueDireita = true
+	
+func _on_cima_pressed():
+	toqueCima = true
+
+func _on_esquerda_pressed():
+	toqueEsquerda = true
