746b6bc555f7192e765eda80ca0aebdb51b75428
otavioluigi07 <Luigi.Macedo@sou.inteli.edu.br>
2023-03-24 08:29:33 -0300

    .

--- src/Projeto_05.8/placario.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-func _on_placario_body_entered(body):
-	print("entrou")
-	if body.is_in_group("jangada"):
-		print("era pra trocar")
-		get_tree().change_scene("res://desafio2.tscn")
