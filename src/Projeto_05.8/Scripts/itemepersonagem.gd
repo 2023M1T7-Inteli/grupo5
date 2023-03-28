ea5df01e68839ea57db70be5eb45f49d2e00b219
otavioluigi07 <Luigi.Macedo@sou.inteli.edu.br>
2023-03-28 14:45:17 -0300

    0.5.8

--- src/Projeto_05.8/Scripts/itemepersonagem.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-#Função que corresponde ao contato entre os corpos (personagem e item).
-func _on_item1_body_entered(body):
-	#Se haver contato entre o item e objetos do grupo "garoto", que nesse caso é o jogador, haverá uma interação. Nesse caso, a troca de mapa.
-	if body.is_in_group("garoto"):
-		get_tree().change_scene("res://Cenas/Desafio1.tscn")
-
