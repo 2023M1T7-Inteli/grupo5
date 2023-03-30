20df79f60893b12d68041c1642608bb8d0ccffc4
Rizzi26 <123962801+Rizzi26@users.noreply.github.com>
2023-03-30 13:23:44 -0300

    0.5.8

--- src/Projeto_05.8/Scripts/personagemdesafio.gd
+++ src/Projeto_05.8/Scripts/personagemdesafio.gd
@@ -6,7 +6,7 @@ var toqueCima = false
 var toqueEsquerda = false
 var listaComandos = []
 var contador = 0
-var iniciar	= false
+var iniciar= false
 var x
 var y
 var listaSetas = []
@@ -46,6 +46,7 @@ func _physics_process(delta):
 			if get_slide_collision(0) != null:
 				self.position.x -= 5	
 			toqueDireita = false
+			iniciar = false
 			_on_iniciar_pressed()
 	
 	if toqueBaixo:
@@ -55,6 +56,7 @@ func _physics_process(delta):
 			if get_slide_collision(0) != null:
 				self.position.y -= 5
 			toqueBaixo = false
+			iniciar = false
 			_on_iniciar_pressed()
 		
 	if toqueCima:
@@ -64,6 +66,7 @@ func _physics_process(delta):
 			if get_slide_collision(0) != null:
 				self.position.y += 5
 			toqueCima = false
+			iniciar = false
 			_on_iniciar_pressed()
 
 	if toqueEsquerda:
@@ -73,57 +76,66 @@ func _physics_process(delta):
 			if get_slide_collision(0) != null:
 				self.position.x += 5
 			toqueEsquerda = false
+			iniciar = false
 			_on_iniciar_pressed()
 		
 	if toqueEsquerda == false and toqueBaixo == false and toqueCima == false and toqueDireita == false:
 		movimento.play("parado")
 	
+	
+func ini():
+	iniciar = true
+	if contador == listaComandos.size():
+		iniciar = false
+		print(iniciar)
+	
 #Função que executa quando clicar o botão de play no console.
 func _on_iniciar_pressed():
-	Global.mao = Global.mao + 1
-	x = $".".position.x
-	y = $".".position.y
-	
-	if contador != listaComandos.size():
-		if listaComandos[contador] == "direita":
-			contador += 1
-			toqueDireita = true
-			
-		elif listaComandos[contador] == "baixo":
-			contador += 1
-			toqueBaixo = true
-			
-		elif listaComandos[contador] == "esquerda":
-			contador += 1
-			toqueEsquerda = true
-			
-		elif listaComandos[contador] == "cima":
-			contador += 1
-			toqueCima = true
-	else:
-		listaComandos = []
-		listaSetas = []
-		contador = 0
-		$"../Direita1".visible = false
-		$"../Direita2".visible = false
-		$"../Direita3".visible = false
-		$"../Direita4".visible = false
-	
-		$"../Esquerda1".visible = false
-		$"../Esquerda2".visible = false
-		$"../Esquerda3".visible = false
-		$"../Esquerda4".visible = false
-	
-		$"../Baixo1".visible = false
-		$"../Baixo2".visible = false
-		$"../Baixo3".visible = false
-		$"../Baixo4".visible = false
-	
-		$"../Cima1".visible = false
-		$"../Cima2".visible = false
-		$"../Cima3".visible = false
-		$"../Cima4".visible = false
-	
+	if iniciar == false:
+		ini()
+		Global.mao = Global.mao + 1
+		x = $".".position.x
+		y = $".".position.y
+		
+		if contador != listaComandos.size():
+			if listaComandos[contador] == "direita":
+				contador += 1
+				toqueDireita = true
+				
+			elif listaComandos[contador] == "baixo":
+				contador += 1
+				toqueBaixo = true
+				
+			elif listaComandos[contador] == "esquerda":
+				contador += 1
+				toqueEsquerda = true
+				
+			elif listaComandos[contador] == "cima":
+				contador += 1
+				toqueCima = true
+		else:
+			listaComandos = []
+			listaSetas = []
+			contador = 0
+			$"../Direita1".visible = false
+			$"../Direita2".visible = false
+			$"../Direita3".visible = false
+			$"../Direita4".visible = false
+		
+			$"../Esquerda1".visible = false
+			$"../Esquerda2".visible = false
+			$"../Esquerda3".visible = false
+			$"../Esquerda4".visible = false
+		
+			$"../Baixo1".visible = false
+			$"../Baixo2".visible = false
+			$"../Baixo3".visible = false
+			$"../Baixo4".visible = false
+		
+			$"../Cima1".visible = false
+			$"../Cima2".visible = false
+			$"../Cima3".visible = false
+			$"../Cima4".visible = false
 #Função que manda o comando BAIXO para o personagem.
 func _on_baixo_pressed():
 	Global.mao = Global.mao + 1
@@ -151,6 +163,8 @@ func _on_esquerda_pressed():
 		listaComandos.append("esquerda")
 		
 func _process(delta):
+	if iniciar:
+		print(iniciar)
 #Função que processa constantemente no mapa.
 	if listaSetas.size() < 5 and listaSetas.size() >= 1:
 		for i in range(len(listaSetas)):
