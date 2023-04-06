f4b5e05e9e85eda45e28205139a37e93151829cf
otavioluigi07 <Luigi.Macedo@sou.inteli.edu.br>
2023-04-06 09:19:09 -0300

    0.6.0

--- src/Projeto_06.0/Projeto_06.0/pescador.gd
+++ src/Projeto_06.0/Projeto_06.0/pescador.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+#Declaração de variáveis
 var play = false
 var iniciar = false
 var colisao = false
@@ -19,7 +20,7 @@ var j = 0
 var voltarBai = false
 var k = 0
 
-
+#Função que apresenta a seta direita no console e adiciona na lista
 func _on_touchDireita_pressed():
 	if iniciar == false:
 		j += 1
@@ -61,6 +62,7 @@ func _on_touchDireita_pressed():
 			k += 1
 			comandos.append("direita")
 
+#Função que apresenta a seta esquerda no console e adiciona na lista
 func _on_touchEsquerda_pressed():
 	if iniciar == false:
 		j += 1
@@ -100,6 +102,7 @@ func _on_touchEsquerda_pressed():
 			k += 1
 			comandos.append("esquerda") #appenda 'esquerda' na lista de comandos.
 
+#Função que apresenta a seta reto no console e adiciona na lista
 func _on_touchAvancar_pressed():
 	if iniciar == false:
 		j += 1
@@ -138,24 +141,30 @@ func _on_touchAvancar_pressed():
 			k += 1
 			comandos.append("avançar")
 
+#Função que executa a função play
 func _on_touchIniciar_pressed(): #botão que lê e executa o console.
 		if (iniciar == false):
 			play()
 
+#Função que rotaciona o personagem no sentido horário
 func girarDireita(): #função de rotação em 90 graus sentido horário.
 	$RayCast2D.cast_to = $RayCast2D.cast_to.rotated(anguloRad)
 
+#Função que rotaciona o personagem no sentido anti-horário
 func girarEsquerda():  #função de rotação em 90 graus sentido anti horário.
 	$RayCast2D.cast_to = $RayCast2D.cast_to.rotated(-anguloRad)
 
+##função que executa o comando reto
 func movimentoReto(): #função intermediária do avanço do personagem.
 	andando = true
 
+#Função que lê uma vez ápos o carregamento do mapa.
 func _ready():
 	$descendo.visible = false
 	$".".visible = false
 	$RayCast2D.cast_to = Vector2.RIGHT #define a direção do eixo x positivo.
 
+#Função que executa constantemente ápos o mapa ser carregado
 func _process(delta):
 
 	if $RayCast2D.cast_to.is_equal_approx(Vector2.DOWN):
@@ -195,7 +204,8 @@ func _process(delta):
 		yield(get_tree().create_timer(0.4),"timeout")
 		andando = false #'desativa' o andando
 
-func parado1():
+
+func parado1(): # função para detectar colisão no eixo y, para baixo, e fazer com que o personagem volte 10 pixels
 	parado = true
 	colisao = false
 	andando = false
@@ -203,7 +213,7 @@ func parado1():
 		self.position.y -= 10
 		parado = false
 
-func parado2():
+func parado2(): # função para detectar colisão no eixo y, para cima, e fazer com que o personagem volte 10 pixels
 	parado = true
 	colisao = false
 	andando = false
@@ -211,7 +221,7 @@ func parado2():
 		self.position.y += 10
 		parado = false
 
-func parado3():
+func parado3(): # função para detectar colisão no eixo x, para direita, e fazer com que o personagem volte 10 pixels
 	parado = true
 	colisao = false
 	andando = false
@@ -219,7 +229,7 @@ func parado3():
 		self.position.x -= 10
 		parado = false
 
-func parado4():
+func parado4(): # função para detectar colisão no eixo x, para o lado direito, e fazer com que o personagem volte 10 pixels
 	parado = true
 	colisao = false
 	andando = false
@@ -227,12 +237,12 @@ func parado4():
 		self.position.x += 10
 		parado = false
 
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(body): # função que detecta se o colison do personagem entrou na parede inisível
 	if body.is_in_group("boneco"):
 		colisao = true
 		print(colisao)
 
-func play():
+func play(): # função que é executada ao ser pressionado o botão iniciar,impedindo que seja adicionado mais elementos no console
 	iniciar = true
 	if iniciar == true:
 		for cont in range(0, comandos.size()):
@@ -263,63 +273,59 @@ func play():
 			$"../espaco10".texture = null
 			j = 0
 
-func _on_Bau1_body_entered(body):
+func _on_Bau1_body_entered(body): # função executada quando o personagem colide com o colision do personagem - bau 1
 	if body.is_in_group("boneco"):
 		$"../PreDesafio1".visible = true
 		$".".visible = false
 
-func _on_PreDesafio_pressed():
+func _on_PreDesafio_pressed(): # função executada quando o personagem colide com o colision do personagem - bau 2
 	$"../PreDesafio1".visible = false
 	$"../DesafioMat1".visible = true
 	$"../Bau1".visible = false
 
-
-
-func _on_RespostaCerta1_pressed():#Resposta errada
+func _on_RespostaCerta1_pressed(): # Resposta errada do bau
 	$"../DesafioMat1".visible = false
 	$"../TenteNovamente".visible = true
 
-func _on_RespostaErrada11_pressed():#Resposta errada
+func _on_RespostaErrada11_pressed(): # Resposta errada do bau
 	$"../DesafioMat1".visible = false
 	$"../TenteNovamente".visible = true
 
-func _on_Resposta_errada_12_pressed():#Resposta certa
+func _on_Resposta_errada_12_pressed(): # Resposta certa do bau
 	$"../DesafioMat2".visible = true
 	$"../DesafioMat1".visible = false
 	$".".visible = false
 	
 	
-func _on_RespostaCerta2_pressed():#Resposta errada
+func _on_RespostaCerta2_pressed(): # Resposta certa do bau 
 	$"../DesafioMat2".visible = false
 	$"../TenteNovamente".visible = true
 
-func _on_RespostaErrada21_pressed():#Resposta certa
+func _on_RespostaErrada21_pressed(): # Resposta errada do bau
 	$"../DesafioMat2".visible = false
 	$"../DesafioMat3".visible = true
 
-func _on_Resposta_errada_22_pressed():#Resposta errada
+func _on_Resposta_errada_22_pressed(): # Resposta errada do bau
 	$"../DesafioMat2".visible = false
 	$"../TenteNovamente".visible = true
 	
-	
-
-func _on_RespostaCerta3_pressed():
+func _on_RespostaCerta3_pressed(): # Resposta certa do bau
 	$"../DesafioMat3".visible = false
 	$"../Premio".visible = true
 
-func _on_RespostaErrada31_pressed():
+func _on_RespostaErrada31_pressed(): # Resposta errada do bau
 	$"../DesafioMat3".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_Resposta_errada_32_pressed():
+func _on_Resposta_errada_32_pressed(): # Resposta errada do bau
 	$"../DesafioMat3".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_TentarDenovo_pressed():
+func _on_TentarDenovo_pressed(): # Resposta errada do bau
 	$"../TenteNovamente2".visible = false
 	$"../DesafioMat1".visible = true
 
-func _on_Premio_pressed():
+func _on_Premio_pressed(): # Ao colocar resposta certa, o usuário ganha um prêmio
 	$"../Premio".visible = false
 	$"../Bau1".visible = false
 	$".".visible = true
@@ -327,70 +333,68 @@ func _on_Premio_pressed():
 
 
 
-func _on_Bau2_body_entered(body):	
+func _on_Bau2_body_entered(body):	# função que abre o baú ao detectar se um corpo entrou em sua área de colisão
 	if body.is_in_group("boneco"):
 		$"../PreDesafio2".visible = true
 		$".".visible = false
 
-func _on_PreDesafio2_pressed():
+func _on_PreDesafio2_pressed(): # função executada quando o personagem colide com o colision do personagem - bau 1
 	$"../PreDesafio2".visible = false
 	$"../DesafioMat4".visible = true
 	
 
-func _on_RespostaCerta12_pressed():#Resposta errada
+func _on_RespostaCerta12_pressed(): # Resposta errada do bau
 	$"../DesafioMat4".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_RespostaErrada112_pressed():#Resposta errada
+func _on_RespostaErrada112_pressed(): # Resposta errada do bau
 	$"../DesafioMat4".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_Resposta_errada_122_pressed():#Resposta certa
+func _on_Resposta_errada_122_pressed(): # Resposta certa do bau
 	$"../DesafioMat5".visible = true
 	$"../DesafioMat4".visible = false
 	$".".visible = false
 	
 	
-func _on_RespostaCerta22_pressed():#Resposta errada
+func _on_RespostaCerta22_pressed(): # Resposta errada do bau
 	$"../DesafioMat5".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_RespostaErrada212_pressed():#Resposta certa
+func _on_RespostaErrada212_pressed(): # Resposta certa do bau
 	$"../DesafioMat5".visible = false
 	$"../DesafioMat6".visible = true
 	
-func _on_Resposta_errada_222_pressed():#Resposta errada
+func _on_Resposta_errada_222_pressed(): # Resposta errada do bau
 	$"../DesafioMat5".visible = false
 	$"../TenteNovamente2".visible = true
 	
-	
-
-func _on_RespostaCerta32_pressed():
+func _on_RespostaCerta32_pressed(): # Resposta certa do bau
 	$"../DesafioMat6".visible = false
 	$"../Premio".visible = true
 
-func _on_Resposta_errada_322_pressed():
+func _on_Resposta_errada_322_pressed(): # Resposta errada do bau
 	$"../DesafioMat6".visible = false
 	$"../TenteNovamente2".visible = true
 
-func _on_TentarDenovo2_pressed():
+func _on_TentarDenovo2_pressed(): # Ao colocar respostas erradas, a função tente novamente aparece na tela
 	$"../DesafioMat4".visible = true
 	$"../TenteNovamente2".visible = false
 
-func _on_Premio2_pressed():
+func _on_Premio2_pressed(): # Premio para o usuário quando acerta a resposta
 	$"../Premio2".visible = false
 	$".".visible = true
 	Global.pontos += 15
 
-func _on_RespostaErrada312_pressed():
+func _on_RespostaErrada312_pressed():  # Resposta errada do bau
 	$"../DesafioMat6".visible = false
 	$"../TenteNovamente2".visible = true
 
 
-func _on_RespostaCerta322_pressed():
+func _on_RespostaCerta322_pressed(): # Resposta certa do bau
 	$"../DesafioMat6".visible = false
 	$"../Premio2".visible = true
 
-func _on_Resposta_errada_3222_pressed():
+func _on_Resposta_errada_3222_pressed(): # Resposta errada do bau
 	$"../DesafioMat6".visible = false
 	$"../TenteNovamente2".visible = true
